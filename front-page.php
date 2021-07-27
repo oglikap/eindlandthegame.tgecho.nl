55e44f502bafb83ae330fa36da0bbc3a1356c37e
u10098p7132 <kroon.rj@gmail.com>
2021-07-27 15:54:00 +0200

    Foto fix?

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
   
   <div class="front-page">
     <div class="uk-container">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/countdown.jpg;" alt="<?php echo bloginfo('title'); ?>">
+    <img src="<?php echo get_theme_file_uri('/images/countdown.jpg'); ?>"; alt="<?php echo bloginfo('title'); ?>">
       <a class="front-page__start" href="<?php echo esc_url(site_url('start')); ?>">Start</a>
     </div>  
    </div>
