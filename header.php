42bbd5d1a9dc6fd3613d2db790b8fb18066a2d35
u10098p7132 <kroon.rj@gmail.com>
2021-08-26 00:01:49 +0200

    Added Plausible

--- header.php
+++ header.php
@@ -4,5 +4,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
+    <script defer data-domain="eindlandthegame.tgecho.com" src="https://plausible.io/js/plausible.js"></script>
   </head>
   <body>
